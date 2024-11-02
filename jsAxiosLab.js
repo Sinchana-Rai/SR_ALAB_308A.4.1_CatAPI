dcc512088a64a7c3bfa8b804e2115e19516d485d
Sinchana-Rai <sinchana.srai@gmail.com>
2024-11-01 22:10:01 -0400

    first commit

---
+++ jsAxiosLab.js
@@ -0,0 +1,17 @@
+
+
+/**
+ * 3. Fork your own sandbox, creating a new one named "JavaScript Axios Lab."
+ */
+/**
+ * 4. Change all of your fetch() functions to axios!
+ * - axios has already been imported for you within index.js.
+ * - If you've done everything correctly up to this point, this should be simple.
+ * - If it is not simple, take a moment to re-evaluate your original code.
+ * - Hint: Axios has the ability to set default headers. Use this to your advantage
+ *   by setting a default header with your API key so that you do not have to
+ *   send it manually with all of your requests! You can also set a default base URL!
+ */
+
+
+    
